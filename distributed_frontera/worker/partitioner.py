1111061f63835784b852477764758b3ad9ff5e59
Alexander Sibiryakov <sixty-one@yandex.ru>
2015-07-30 19:22:30 +0200

    Checking argument if present, if not using object-level.

--- distributed_frontera/worker/partitioner.py
+++ distributed_frontera/worker/partitioner.py
@@ -11,15 +11,15 @@ class FingerprintPartitioner(Partitioner):
         digest = unhexlify(key[0:2] + key[5:7] + key[10:12] + key[15:17])
         value = unpack("<I", digest)
         idx = value[0] % size
-        return self.partitions[idx]
+        return self.partitions[idx] if partitions else self.partitions[idx]
 
 
 class Crc32NamePartitioner(Partitioner):
     def partition(self, key, partitions):
         value = crc32(key) if type(key) is str else crc32(key.encode('utf-8', 'ignore'))
-        return self.partition_by_hash(value, partitions)
+        return self.partition_by_hash(value, partitions if partitions else self.partitions)
 
     def partition_by_hash(self, value, partitions):
         size = len(self.partitions)
         idx = value % size
-        return self.partitions[idx]
+        return partitions[idx]

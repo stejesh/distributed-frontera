d688710d85cca98fe74909e0e54e00d87ccd5f30
Alexander Sibiryakov <sixty-one@yandex.ru>
2015-08-04 16:47:18 +0200

    more work

--- distributed_frontera/messagebus/namedpipes.py
+++ distributed_frontera/messagebus/namedpipes.py
@@ -4,8 +4,10 @@ from base import BaseSpiderFeedStream, BaseStreamConsumer
 from distributed_frontera.worker.partitioner import Crc32NamePartitioner
 from os import mkfifo, open, close, read, write, O_NONBLOCK, O_RDONLY, O_WRONLY
 from os.path import exists
-from struct import pack
+from struct import pack, unpack
 from errno import EAGAIN, EWOULDBLOCK
+from io import BytesIO
+from time import sleep, clock
 
 
 ROOT = "/Users/sibiryakov/local/frontera"
@@ -16,20 +18,38 @@ def get_path(partition):
 class StreamConsumer(BaseStreamConsumer):
     def __init__(self, partition_id):
         self.fd = open(get_path(partition_id), O_NONBLOCK | O_RDONLY)
+        self.buffer = BytesIO()
 
     def get_message(self, timeout=0.1, count=1):
-        try:
-            buffer = read(self.fd, 4096)
-        except OSError as err:
-            if err.errno == EAGAIN or err.errno == EWOULDBLOCK:
-                buffer = None
-            else:
-                raise  # something else has happened -- better reraise
+        result = []
+        start_time = clock()
+        while len(result) < count:
+            try:
+                buffer = read(self.fd, 4096)
+            except OSError as err:
+                if err.errno == EAGAIN or err.errno == EWOULDBLOCK:
+                    buffer = None
+                else:
+                    raise
 
-        if buffer is None:
-            # nothing was received -- do something else
-        else:
-            # buffer contains some received data -- do something with it
+            if buffer is None:
+                if start_time + timeout < clock():
+                    sleep(0.1)
+                    continue
+                else:
+                    return result
+
+            self.buffer.write(buffer)
+
+
+        stream = BytesIO(self.buffer)
+        length = unpack('>I', stream.read(4))[0]
+        message = stream.read(length)
+        result.append(message)
+
+
+
+
 
     def close(self):
         close(self.fd)

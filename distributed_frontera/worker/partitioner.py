b73d37a5755c4e63545a5a3743820eb473711f46
Alexander Sibiryakov <sixty-one@yandex.ru>
2015-08-06 19:30:19 +0200

    Right method signature for latest kafka-python v0.9.4

--- distributed_frontera/worker/partitioner.py
+++ distributed_frontera/worker/partitioner.py
@@ -6,7 +6,7 @@ from kafka.partitioner.base import Partitioner
 
 
 class FingerprintPartitioner(Partitioner):
-    def partition(self, key, partitions):
+    def partition(self, key, partitions=None):
         if not partitions:
             partitions = self.partitions
         digest = unhexlify(key[0:2] + key[5:7] + key[10:12] + key[15:17])
@@ -16,7 +16,7 @@ class FingerprintPartitioner(Partitioner):
 
 
 class Crc32NamePartitioner(Partitioner):
-    def partition(self, key, partitions):
+    def partition(self, key, partitions=None):
         value = crc32(key) if type(key) is str else crc32(key.encode('utf-8', 'ignore'))
         return self.partition_by_hash(value, partitions if partitions else self.partitions)
 
